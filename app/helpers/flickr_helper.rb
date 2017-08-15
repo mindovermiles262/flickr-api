8c528aba830f01187d75e0c57dd96c051acf80d3
Andy Duss <mindovermiles262@gmail.com>
2017-08-15 13:18:43 -0700

    add username search

--- app/helpers/flickr_helper.rb
+++ app/helpers/flickr_helper.rb
@@ -1,15 +1,23 @@
 module FlickrHelper
-  def user_photos(user_id, photo_count = 12)
+  def user_photos(user_id, photo_count = 20)
     flickr.photos.search(:user_id => user_id).first(photo_count)
   end
 
-  def render_flickr_sidebar_widget(user_id, photo_count = 12, columns = 2)
+  def get_id_from_username(username)
     begin
-      photos = user_photos(user_id, photo_count).in_groups_of(2)
-      render  :partial => '/flickr/sidebar_widget', 
+      flickr.people.findByUsername(:username => username)     
+    rescue => exception
+      render :partial => '/flickr/user_not_found'
+    end
+  end
+
+  def render_flickr_stream(user_id, photo_count = 20, columns = photo_count)
+    begin
+      photos = user_photos(user_id, photo_count).in_groups_of(columns)
+      render  :partial => '/flickr/flickr_widget', 
               :locals => { :photos => photos }
     rescue => exception
-      render :partial => '/flickr/unavailable'
+      render :partial => '/flickr/user_not_found'
     end
   end
 end
